42a74bba1776122180ba6862dd5b6d8f45481ff7
Nafmo2 <nafmo17@gmail.com>
2018-03-02 22:01:27 +0900

    マス移動(グローバル)完成 ※関数化必須 0302

--- Main.cpp
+++ Main.cpp
@@ -1,34 +1,37 @@
 ﻿
-#include "Main.h"
+#include "Head.h"
 void Main()
 {
 	const Font font(30);
-	double x=100, y=100,dx=0,g=0.01,a=0,px;
-	bool M=false;
+	double x=100, y=100,dx=0,px=0;
+	bool M=false,N=false;
 	while (System::Update())
 	{
 		Circle(x, y,20).draw(Color(0,255,255));
-		Circle(100, 140, 20).draw(Color(0, 255, 255));
-		Circle(150, 140, 20).draw(Color(0, 255, 255));
+		Circle(100, 140, 20).draw(Color(0, 255, 0));
+		Circle(150, 140, 20).draw(Color(0, 255, 0));
 		Line(0, 80, 640, 80).draw(1);
 		Line(0, 120, 640, 120).draw(1);
 
 		if (Input::KeyRight.clicked) {
-			if (!M) {
+			if (!M&&!N) {
 				M = true;
-				a = 0,g=0.8, dx = 1,px=x;
+				dx = 1,px=x;
 			}
 		}
-		if (M) {
-			if (x < px+50) {
-				x += dx;
+		if (Input::KeyLeft.clicked) {
+			if (!M && !N) {
+				N = true;
+				dx = 1, px = x;
+			}
+		}
+		if (M||N) {
+			if ((M?x<px+50:x>px-50)) {
+				x += (M ? dx : -dx);
 			}else {
-				M = false;
+				M = N = false;
 			}
 		}
-
-		if (Input::KeyLeft.clicked)x -= 50;
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
 		font(L"x:",x,L" dx:",dx,L" M:",M).draw();
 	}
 }
