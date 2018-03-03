03aa124907f7f6f5a9ea2cd1bc9325a3948d92d6
Nafmo2 <nafmo17@gmail.com>
2018-03-03 23:17:15 +0900

    MakeEnemyCircle

--- Main.cpp
+++ Main.cpp
@@ -3,15 +3,19 @@ void Main()
 {
 	const Font font(30);
 	Player P;
+	Enemy  E;
 	while (System::Update())
 	{
+
+		P.move();
+
 		Circle(P.GetX(), P.GetY(),20).draw(Color(0,255,255));
+		Circle(E.GetX(), E.GetY(), 100).draw(Palette::Orange);
+		font(L"Enemy?").drawCenter(E.GetX(), E.GetY(), Palette::White);
 		for (int i = 1; i <= 16; i++) {
 			Line(0, 40*i, 640, 40*i).draw(1);
 			Line(40*i, 0, 40*i, 480).draw(1);
 		}
-
-		P.move();
 		font(L"x:",P.GetX(),L" y:",P.GetY()).draw();
 	}
 }
